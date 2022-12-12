a223dedf1f177a6551233789dfccd0b075d9c823
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-12-12 19:02:42 -0300

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      spacing: {
+        "2/3": "66.666667%",
+      },
+      backgroundColor: {
+        "main-bg": "#FAFBFB",
+        "main-dark-bg": "#20232A",
+        "secondary-dark-bg": "#33373E",
+        "light-gray": "#F7F7F7",
+        "half-transparent": "rgba(0, 0, 0, 0.5)",
+      },
+    },
+  },
+  plugins: [],
+};
