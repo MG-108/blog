4bc55ba49ed1a6f4002f0a4602aee6ac6cebfaff
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-12-25 20:24:06 -0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,26 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
 import { ContextProvider } from "../context/contextProvider";
 import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/router";
+import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange>
       <ContextProvider>
