363aaba5223506b623279f44b4a492a45d018441
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-12-26 16:47:14 -0300

    pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Layout } from "../components";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
 import { ContextProvider } from "../context/contextProvider";
 import { ThemeProvider } from "next-themes";
-import { useRouter } from "next/router";
-import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange>
       <ContextProvider>
