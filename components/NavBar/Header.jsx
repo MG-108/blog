e36b751b2cd06da95128f1ffed4fd6e9c37f7037
Matheus Ghignatti <matheusghignatti@gmail.com>
2023-02-03 15:00:05 -0300

    refactoring mobile nav

--- components/NavBar/Header.jsx
+++ components/NavBar/Header.jsx
@@ -1,41 +1,49 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { useStateContext } from '../../context/contextProvider';
-import { NavLinks, NavMenuSm, Theme } from '..';
-import { GrClose } from 'react-icons/gr';
-import { BiMenu } from 'react-icons/bi';
-import logo from '../../public/Logo.svg';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useStateContext } from "../../context/contextProvider";
+import { NavLinks, NavMenuSm, Theme } from "..";
+import { GrClose } from "react-icons/gr";
+import { BiMenu } from "react-icons/bi";
+import logo from "../../public/Logo.svg";
 
 const Header = () => {
   const { open, setOpen } = useStateContext();
 
   return (
-    <div className="fixed w-screen z-10 top-0">
+    <div className="fixed w-screen z-10 top-0 ">
       <div className=" bg-orange-500 shadow-lg   px-4 md:px-8 lg:px-16   ">
         <div className="flex items-center justify-between py-5 ">
-          <div className="md:hidden">
-            <button onClick={() => setOpen(!open)} className="text-3xl cursor-pointer md:hidden">
+          {/* MOBILE MENU BUTTON (close, open) */}
+          <div className="md:hidden ">
+            <button
+              onClick={() => setOpen((prevOpen) => !prevOpen)}
+              className="text-3xl cursor-pointer "
+            >
               <span>{open ? <GrClose size={30} /> : <BiMenu size={30} />}</span>
             </button>
           </div>
-          <div>
-            <Link href="/">
-              <div className="flex items-center">
-                <span>
-                  <Image
-                    src={logo}
-                    alt="logo do cripto area"
-                    className="rounded-full md:w-16 md:h-16  w-12 h-12"
-                  />
-                </span>
-                <span className=" pl-2 cursor-pointer font-bold font-mono text-xl md:text-4xl text-white hover:text-black transition duration-500 ease">
-                  Cripto Area
-                </span>
-              </div>
-            </Link>
-          </div>
+          {/* NAV LOGO AND NAME */}
+
+          <Link href="/">
+            <div className="flex items-center">
+              <span>
+                <Image
+                  src={logo}
+                  alt="logo do cripto area"
+                  className="rounded-full md:w-16 md:h-16  w-12 h-12"
+                />
+              </span>
+              <span className=" pl-2 cursor-pointer font-bold font-mono text-xl md:text-4xl text-white  ">
+                Cripto Area
+              </span>
+            </div>
+          </Link>
+
+          {/* CATEGORIES NAVLINKS */}
           <NavLinks />
+
+          {/* THEME SELECT */}
           <div>
             <div className="cursor-pointer ">
               <Theme />
@@ -43,7 +51,10 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="md:hidden">{open ? <NavMenuSm /> : null}</div>
+      {/* MOBILE SIDEBAR MENU */}
+      <div className="md:hidden relative ">
+        <NavMenuSm />{" "}
+      </div>
     </div>
   );
 };
--- components/NavBar/NavMenuSm.jsx
+++ components/NavBar/NavMenuSm.jsx
@@ -1,33 +1,38 @@
-import React, { useEffect } from 'react';
-import Link from 'next/link';
-import { useStateContext } from '../../context/contextProvider';
-import { getCategories } from '../../services';
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { useStateContext } from "../../context/contextProvider";
+import { getCategories } from "../../services";
+import { GrClose } from "react-icons/gr";
+import { BiMenu } from "react-icons/bi";
 
 const NavMenuSm = () => {
-  const navLinks = [{ name: 'Sobre', link: '/sobre' }];
+  const navLinks = [{ name: "Sobre", link: "/sobre" }];
 
-  const { setCategories, categories } = useStateContext();
+  const { setCategories, categories, open, setOpen } = useStateContext();
   useEffect(() => {
     getCategories().then((newCategories) => setCategories(newCategories));
   }, []);
 
   return (
-    <div className="  h-full w-[300px] bg-orange-500 shadow-2xl rounded-b-lg z-10 fixed ">
+    <div
+      className={`smooth-transition fixed h-screen top-21 z-10  w-2/3  from-white/5 to-orange-500
+         p-5 backdrop-blur-2xl  bg-gradient-to-tl ${open ? "left-0" : "-left-full"}`}
+    >
       <div className="flex flex-col ml-6">
         <div className="flex flex-col">
           {navLinks.map((link) => (
             <div key={link.name}>
               <Link
                 href={link.link}
-                className="text-lg font-bold font-mono text-white hover:text-black duration-250 "
+                className=" font-bold font-mono text-white hover:text-black duration-250 "
               >
                 {link.name}
               </Link>
             </div>
           ))}
           {categories.map((category) => (
-            <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="text-lg font-semibold font-mono text-white hover:text-black duration-250 ">
+            <Link key={category.slug} href={`/category/${category.slug}`} className="mt-2">
+              <span className=" font-semibold font-mono text-white hover:text-black duration-250 ">
                 {category.name}
               </span>
             </Link>
